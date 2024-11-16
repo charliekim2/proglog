d5cd491ab1e4adc9319bc9ceb5ac27972827e6b8
Charlie Kim <charliekim451@gmail.com>
2024-11-16 13:59:34 -0700

    log done

--- internal/log/log.go
+++ internal/log/log.go
@@ -1,12 +1,16 @@
 package log
 
 import (
+	"fmt"
+	"io"
 	"os"
 	"path"
 	"sort"
 	"strconv"
 	"strings"
 	"sync"
+
+	api "proglog/api/v1"
 )
 
 type Log struct {
@@ -71,6 +75,130 @@ func (l *Log) setup() error {
 	}
 
 	return nil
+}
+
+func (l *Log) Append(record *api.Record) (uint64, error) {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+
+	off, err := l.activeSegment.Append(record)
+	if err != nil {
+		return 0, err
+	}
+
+	if l.activeSegment.IsMaxed() {
+		err = l.newSegment(off + 1)
+	}
+
+	return off, err
+}
+
+func (l *Log) Read(off uint64) (*api.Record, error) {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+
+	var s *segment
+	for _, seg := range l.segments {
+		if seg.baseOffset <= off && seg.nextOffset > off {
+			s = seg
+			break
+		}
+	}
+
+	if s == nil || s.nextOffset <= off {
+		return nil, fmt.Errorf("offset %d out of range", off)
+	}
+
+	return s.Read(off)
+}
+
+func (l *Log) Close() error {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+
+	for _, seg := range l.segments {
+		if err := seg.Close(); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (l *Log) Remove() error {
+	if err := l.Close(); err != nil {
+		return err
+	}
+	return os.RemoveAll(l.Dir)
+}
+
+func (l *Log) Reset() error {
+	if err := l.Close(); err != nil {
+		return err
+	}
+
+	return l.setup()
+}
+
+func (l *Log) LowestOffset() (uint64, error) {
+	l.mu.RLock()
+	defer l.mu.RUnlock()
+
+	return l.segments[0].baseOffset, nil
+}
+
+func (l *Log) HighestOffset() (uint64, error) {
+	l.mu.RLock()
+	defer l.mu.RUnlock()
+
+	off := l.segments[len(l.segments)-1].nextOffset
+	if off == 0 {
+		return 0, nil
+	}
+
+	return off - 1, nil
+}
+
+func (l *Log) Truncate(lowest uint64) error {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+
+	var segments []*segment
+	for _, s := range l.segments {
+		if s.nextOffset <= lowest+1 {
+			if err := s.Remove(); err != nil {
+				return err
+			}
+
+			continue
+		}
+		segments = append(segments, s)
+	}
+	l.segments = segments
+
+	return nil
+}
+
+func (l *Log) Reader() io.Reader {
+	l.mu.RLock()
+	defer l.mu.RUnlock()
+
+	readers := make([]io.Reader, len(l.segments))
+	for i, seg := range l.segments {
+		readers[i] = &originReader{seg.store, 0}
+	}
+	return io.MultiReader(readers...)
+}
+
+type originReader struct {
+	*store
+	off int64
+}
+
+func (o *originReader) Read(p []byte) (int, error) {
+	n, err := o.ReadAt(p, o.off)
+	o.off += int64(n)
+	return n, err
 }
 
 func (l *Log) newSegment(off uint64) error {
